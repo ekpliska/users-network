f3b7e881683b73f90dea9ead17973cf7b5005030
ekpliska <pliska.ek@gmail.com>
2019-08-14 22:38:31 +0300

    changed: the users component to Class, added componentDidMount()

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,33 +2,39 @@ import React from 'react';
 import User from './User';
 import * as axios from 'axios';
 
-const Users = (props) => {
-    const { users, isLoading, followClick, unFollowClick, allUsers } = props;
-    if (isLoading === false) {
+class Users extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then((response) => {
-                allUsers(response.data.items)
+                this.props.allUsers(response.data.items)
             });
-        
     }
 
-    return (
-        <div>
-            {
-                (isLoading === false)
-                    ? <p>Users are loading... wait</p>
-                    : users.map((user) => {
-                        return <User
-                            key={user.id}
-                            {...user}
-                            followClick={followClick}
-                            unFollowClick={unFollowClick}
-                            allUsers={allUsers}
-                        />
-                    })
-            }
-        </div>
-    )
+    render() {
+        const { users, isLoading, followClick, unFollowClick, allUsers } = this.props;
+        return (
+            <div>
+                {
+                    (isLoading === false)
+                        ? <p>Users are loading... wait</p>
+                        : users.map((user) => {
+                            return <User
+                                key={user.id}
+                                {...user}
+                                followClick={followClick}
+                                unFollowClick={unFollowClick}
+                                allUsers={allUsers}
+                            />
+                        })
+                }
+            </div>
+        )
+    }
 }
 
 export default Users;
